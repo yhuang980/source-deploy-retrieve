80a8e6e5244d738a3c17193481ab203efd7fa7e9
Shelby Holden <shelby.holden@salesforce.com>
2021-05-06 07:51:15 -0600

    chore: update pipeline parameter name to be unique from job name

--- scripts/update-registry-workflow.sh
+++ scripts/update-registry-workflow.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-# For publishing a different version, see the format below for adding a publish-type
-# parameter:
-# "publish-type": "minor"
-
 CircleCIToken=$1
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "enableRegistryBot",
   "parameters": {
-    "update-registry": true,
+    "run-update-registry": true,
   }
 }' https://circleci.com/api/v2/project/gh/forcedotcom/source-deploy-retrieve/pipeline

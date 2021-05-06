a687df5db78caf8a6e39a3b4a39f10a927bbde1d
Shelby Holden <shelby.holden@salesforce.com>
2021-05-06 07:56:44 -0600

    chore: remove comma from parameters list

--- scripts/update-registry-workflow.sh
+++ scripts/update-registry-workflow.sh
@@ -4,6 +4,6 @@ CircleCIToken=$1
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "enableRegistryBot",
   "parameters": {
-    "run-update-registry": true,
+    "run-update-registry": true
   }
 }' https://circleci.com/api/v2/project/gh/forcedotcom/source-deploy-retrieve/pipeline

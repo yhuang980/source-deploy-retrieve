513b47d6b3048ca436b0852ffc63a2180d2f0940
Shelby Holden <shelby.holden@salesforce.com>
2021-05-07 14:34:19 -0600

    chore: revert to default settings and branch

--- scripts/update-registry-workflow.sh
+++ scripts/update-registry-workflow.sh
@@ -2,7 +2,7 @@
 
 CircleCIToken=$1
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
-  "branch": "enableRegistryBot",
+  "branch": "develop",
   "parameters": {
     "run-update-registry": true
   }
